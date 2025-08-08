f11acabcaf66eb21d0e6a31afea43468ff2f986f
yanagiragi <qaz90473@gmail.com>
2025-08-09 00:17:59 +0800

    fix: Fix asset path incorrect & blank page in gh-page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import ImageCompressor from '../views/ImageCompressor.vue'
 import LongExposure from '../views/LongExposure.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
