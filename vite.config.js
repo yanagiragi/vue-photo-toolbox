9201dae6a0f4ae6dbf02ffc74a8b204f78474ad0
yanagiragi <qaz90473@gmail.com>
2025-08-10 14:04:10 +0800

    feat: Support PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,20 @@ export default defineConfig({
           dest: ''  // 直接丟到根目錄
         }
       ]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate', // 自動更新
+      includeAssets: ['pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Photo ToolBox',
+        short_name: 'PT',
+        description: 'Photo ToolBox',
+        theme_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
     })
   ],
   base: '/vue-photo-toolbox/',
